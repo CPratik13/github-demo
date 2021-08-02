39a094e8b4293f8ecbe5340e3bac14323eb33b63
CPratik13 <47912084+CPratik13@users.noreply.github.com>
2021-08-02 14:31:14 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#add implememtation
+def add(x,y):
+    pass
+
+#subtract implementation
+def subtract(x,y):
+    pass
+
+#multiple implementation
+def multiple(x,y):
+    pass
+
+#divide implementation
+def divide(x,y):
+    pass
